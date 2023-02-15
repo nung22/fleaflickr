1126e6a28ddd5b2f2275763d8fe5eef9c737170e
Nicholas Ung <37952714+nung22@users.noreply.github.com>
2023-02-14 23:33:48 -0800

    Update controllers

--- src/main/java/com/nicholasung/fleaflickr/controllers/ProjectController.java
+++ src/main/java/com/nicholasung/fleaflickr/controllers/ProjectController.java
@@ -20,149 +20,160 @@ import com.nicholasung.fleaflickr.models.Ticket;
 import com.nicholasung.fleaflickr.models.User;
 import com.nicholasung.fleaflickr.services.ProjectService;
 import com.nicholasung.fleaflickr.services.TicketService;
+import com.nicholasung.fleaflickr.services.UserService;
 
 @Controller
 public class ProjectController {
-	
-	   @Autowired
-	   private ProjectService projectServ;
-	   
-	   @Autowired
-	   private TicketService ticketServ;
-	   
-	   // DASHBOARD
-	   @GetMapping("/projects")
-	   public String home(HttpSession session, Model model) {
-		   if(session.getAttribute("user") == null) {
-			   return "redirect:/logout";
-		   }
-		   User user = (User) session.getAttribute("user");
-		   List<Project> projectsNotOnTeam = projectServ.allProjectsNotOnTeam(user);
-		   List<Project> projectsOnTeam = projectServ.allProjectsOnTeam(user);
-		   model.addAttribute("projectsNotOnTeam", projectsNotOnTeam);
-		   model.addAttribute("projectsOnTeam", projectsOnTeam);
-		   return "projects.jsp";
-	   }
-	   // Join Team
-	   @GetMapping("/join/{userId}/{projectId}")
-	   public String joinTeam(@PathVariable("userId") Long userId, 
-			   @PathVariable("projectId") Long projectId) {
-		   projectServ.addUserToProject(projectId, userId);
-		   return "redirect:/projects";
-	   }
-	   // Leave Team
-	   @GetMapping("/leave/{userId}/{projectId}")
-	   public String leaveTeam(@PathVariable("userId") Long userId, 
-			   @PathVariable("projectId") Long projectId) {
-		   projectServ.removeUserFromProject(projectId, userId);
-		   return "redirect:/projects";
-	   }
-	   
-	   // SHOW PROJECT INFO
-	   @GetMapping("/projects/{id}")
-	   public String show(HttpSession session, @PathVariable("id") Long id, 
-			   Model model) {
-		   if(session.getAttribute("user") == null) {
-			   return "redirect:/logout";
-		   }
-		   User user = (User) session.getAttribute("user");
-		   model.addAttribute("joinedProject", projectServ.isOnProject(user, id));
-		   Project project = projectServ.findProject(id);
-		   model.addAttribute("project", project);
-		   return "show.jsp";
-	   }
-	   
-	   // ADD PROJECT
-	   @GetMapping("/projects/new")
-	   public String addProject(HttpSession session, 
-			   @ModelAttribute("project") Project emptyProject) {
-		   if(session.getAttribute("user") == null) {
-			   return "redirect:/logout";
-		   }
-		   return "create.jsp";
-	   }
-	   // Add Project
-	   @PostMapping("/projects/new")
-	   public String addProject(HttpSession session,
-			   @Valid @ModelAttribute("project") Project filledProject, 
-			   BindingResult result) {
-		   if(result.hasErrors()) {		   
-			   return "create.jsp";
-		   }
-		   User user = (User) session.getAttribute("user");
-		   projectServ.createProject(filledProject);
-		   projectServ.addUserToProject(filledProject.getId(), user.getId());
-		   return "redirect:/projects";
-	   }
-	   
-	   // EDIT PROJECT
-	   @GetMapping("/projects/edit/{id}")
-	   public String editProject(HttpSession session, 
-			   @PathVariable("id") Long id, Model model) {
-		   if(session.getAttribute("user") == null) {
-			   return "redirect:/logout";
-		   }
-		   Project project = projectServ.findProject(id);
-		   model.addAttribute("project", project);
-		   return "edit.jsp";
-	   }
-	   // Edit Project
-	   @PutMapping("/projects/edit/{id}")
-	   public String editProject(HttpSession session, @PathVariable("id") Long id,
-			   Model model, @Valid @ModelAttribute("project") Project editedProject,
-			   BindingResult result) {
-		   if(result.hasErrors()) {
-			   model.addAttribute("project", editedProject);
-			   return "edit.jsp";
-		   }
-		   User user = (User) session.getAttribute("user");
-		   projectServ.updateProject(editedProject);
-		   projectServ.addUserToProject(editedProject.getId(), user.getId());
-		   return "redirect:/projects";
-	   }
-	   
-	   // DELETE PROJECT
-	   @GetMapping("/projects/{id}/delete")
-	   public String deleteProject(@PathVariable("id") Long id) {
-			projectServ.deleteProject(id);
-			return "redirect:/projects";
-	   }
-	   
-	   // ADD TASK
-	   @GetMapping("/projects/{id}/tickets")
-	   public String addTicket(HttpSession session, Model model,
-			   @PathVariable("id") Long id,
-			   @ModelAttribute("ticket") Ticket emptyTicket) {
-		   if(session.getAttribute("user") == null) {
-			   return "redirect:/logout";
-		   }
-		   Project project = projectServ.findProject(id);
-		   List<Ticket> tickets = ticketServ.allProjectTickets(id);
-		   model.addAttribute("project", project);
-		   model.addAttribute("tickets", tickets);
-		   return "tickets.jsp";
-	   }
-	   // Add Ticket
-	   @PostMapping("/projects/{projectId}/tickets")
-	   public String addTicket(@PathVariable("projectId") Long projectId, Model model,
-			   @Valid @ModelAttribute("ticket") Ticket filledTicket, 
-			   BindingResult result) {
-		   if(result.hasErrors()) {	
-			   Project project = projectServ.findProject(projectId);
-			   List<Ticket> tickets = ticketServ.allProjectTickets(projectId);
-			   model.addAttribute("project", project);
-			   model.addAttribute("tickets", tickets);
-			   return "tickets.jsp";
-		   }
-		   ticketServ.createTicket(filledTicket);
-		   return "redirect:/projects/{projectId}/tickets";
-	   }
-	   
-	   // DELETE TASK
-	   @GetMapping("/projects/{projectId}/tickets/{ticketId}/delete")
-	   public String deleteTicket(@PathVariable("projectId") Long projectId, 
-			   @PathVariable("ticketId") Long ticketId) {
-			ticketServ.deleteTicket(ticketServ.findById(ticketId));
-			return "redirect:/projects/{projectId}/tickets";
-	   }
+
+	@Autowired
+	private UserService userServ;
+
+	@Autowired
+	private ProjectService projectServ;
+
+	@Autowired
+	private TicketService ticketServ;
+
+	// DASHBOARD
+	@GetMapping("/projects")
+	public String home(HttpSession session, Model model) {
+		if (session.getAttribute("user") == null) {
+			return "redirect:/logout";
+		}
+		User user = (User) session.getAttribute("user");
+		List<Project> projectsNotOnTeam = projectServ.allProjectsNotOnTeam(user);
+		List<Project> projectsOnTeam = projectServ.allProjectsOnTeam(user);
+		model.addAttribute("projectsNotOnTeam", projectsNotOnTeam);
+		model.addAttribute("projectsOnTeam", projectsOnTeam);
+		return "projects.jsp";
+	}
+
+	// Join Team
+	@GetMapping("/join/{userId}/{projectId}")
+	public String joinTeam(@PathVariable("userId") Long userId,
+			@PathVariable("projectId") Long projectId) {
+		projectServ.addUserToProject(projectId, userId);
+		return "redirect:/projects";
+	}
+
+	// Leave Team
+	@GetMapping("/leave/{userId}/{projectId}")
+	public String leaveTeam(@PathVariable("userId") Long userId,
+			@PathVariable("projectId") Long projectId) {
+		projectServ.removeUserFromProject(projectId, userId);
+		return "redirect:/projects";
+	}
+
+	// SHOW PROJECT INFO
+	@GetMapping("/projects/{id}")
+	public String show(HttpSession session, @PathVariable("id") Long id,
+			Model model) {
+		if (session.getAttribute("user") == null) {
+			return "redirect:/logout";
+		}
+		User user = (User) session.getAttribute("user");
+		model.addAttribute("joinedProject", projectServ.isOnProject(user, id));
+		Project project = projectServ.findProject(id);
+		model.addAttribute("project", project);
+		return "show.jsp";
+	}
+
+	// ADD PROJECT
+	@GetMapping("/projects/new")
+	public String addProject(HttpSession session,
+			@ModelAttribute("project") Project emptyProject) {
+		if (session.getAttribute("user") == null) {
+			return "redirect:/logout";
+		}
+		return "create.jsp";
+	}
+
+	// Add Project
+	@PostMapping("/projects/new")
+	public String addProject(HttpSession session,
+			@Valid @ModelAttribute("project") Project filledProject,
+			BindingResult result) {
+		if (result.hasErrors()) {
+			return "create.jsp";
+		}
+		User user = (User) session.getAttribute("user");
+		projectServ.createProject(filledProject);
+		projectServ.addUserToProject(filledProject.getId(), user.getId());
+		return "redirect:/projects";
+	}
+
+	// EDIT PROJECT
+	@GetMapping("/projects/edit/{id}")
+	public String editProject(HttpSession session,
+			@PathVariable("id") Long id, Model model) {
+		if (session.getAttribute("user") == null) {
+			return "redirect:/logout";
+		}
+		Project project = projectServ.findProject(id);
+		model.addAttribute("project", project);
+		return "edit.jsp";
+	}
+
+	// Edit Project
+	@PutMapping("/projects/edit/{id}")
+	public String editProject(HttpSession session, @PathVariable("id") Long id,
+			Model model, @Valid @ModelAttribute("project") Project editedProject,
+			BindingResult result) {
+		if (result.hasErrors()) {
+			model.addAttribute("project", editedProject);
+			return "edit.jsp";
+		}
+		User user = (User) session.getAttribute("user");
+		projectServ.updateProject(editedProject);
+		projectServ.addUserToProject(editedProject.getId(), user.getId());
+		return "redirect:/projects";
+	}
+
+	// DELETE PROJECT
+	@GetMapping("/projects/{id}/delete")
+	public String deleteProject(@PathVariable("id") Long id) {
+		projectServ.deleteProject(id);
+		return "redirect:/projects";
+	}
+
+	// ADD TICKET
+	@GetMapping("/projects/{id}/tickets")
+	public String addTicket(HttpSession session, Model model,
+			@PathVariable("id") Long id,
+			@ModelAttribute("ticket") Ticket emptyTicket) {
+		if (session.getAttribute("user") == null) {
+			return "redirect:/logout";
+		}
+		Project project = projectServ.findProject(id);
+		List<Ticket> tickets = ticketServ.allProjectTickets(id);
+		List<User> allUsers = userServ.allUsers();
+		model.addAttribute("project", project);
+		model.addAttribute("allUsers", allUsers);
+		model.addAttribute("tickets", tickets);
+		return "tickets.jsp";
+	}
+
+	// Add Ticket
+	@PostMapping("/projects/{projectId}/tickets")
+	public String addTicket(@PathVariable("projectId") Long projectId, Model model,
+			@Valid @ModelAttribute("ticket") Ticket filledTicket,
+			BindingResult result) {
+		if (result.hasErrors()) {
+			Project project = projectServ.findProject(projectId);
+			List<Ticket> tickets = ticketServ.allProjectTickets(projectId);
+			model.addAttribute("project", project);
+			model.addAttribute("tickets", tickets);
+			return "tickets.jsp";
+		}
+		ticketServ.createTicket(filledTicket);
+		return "redirect:/projects/{projectId}/tickets";
+	}
+
+	// DELETE TICKET
+	@GetMapping("/projects/{projectId}/tickets/{ticketId}/delete")
+	public String deleteTicket(@PathVariable("projectId") Long projectId,
+			@PathVariable("ticketId") Long ticketId) {
+		ticketServ.deleteTicket(ticketId);
+		return "redirect:/projects/{projectId}/tickets";
+	}
 }
--- src/main/java/com/nicholasung/fleaflickr/controllers/UserController.java
+++ src/main/java/com/nicholasung/fleaflickr/controllers/UserController.java
@@ -17,69 +17,70 @@ import com.nicholasung.fleaflickr.services.UserService;
 
 @Controller
 public class UserController {
-	
-	   @Autowired
-	   private UserService userServ;
-	   
-	   // LOGIN & REGISTRATION
-	   @GetMapping("/")
-	   public String index(Model model) {
-	       // Bind empty User and LoginUser objects to the JSP
-	       // to capture the form input
-	       model.addAttribute("newUser", new User());
-	       model.addAttribute("newLogin", new LoginUser());
-	       return "index.jsp";
-	   }
-	   // Registration
-	   @PostMapping("/register")
-	   public String register(@Valid @ModelAttribute("newUser") User newUser, 
-	           BindingResult result, Model model, HttpSession session) {
-	       
-	       // TO-DO Later -- call a register method in the service 
-	       // to do some extra validations and create a new user!
-	       userServ.register(newUser, result);
-	   	
-	       if(result.hasErrors()) {
-	           // Be sure to send in the empty LoginUser before 
-	           // re-rendering the page.
-	           model.addAttribute("newUser", newUser);
-	           model.addAttribute("newLogin", new LoginUser());
-	           return "index.jsp";
-	       }
-	       
-	       // No errors! 
-	       // TO-DO Later: Store their ID from the DB in session, 
-	       // in other words, log them in.
-	       session.setAttribute("user", newUser);
-	       return "redirect:/home";
-	   }
-	   // Login
-	   @PostMapping("/login")
-	   public String login(@Valid @ModelAttribute("newLogin") LoginUser newLogin, 
-	           BindingResult result, Model model, HttpSession session) {
-	       
-	       // Add once service is implemented:
-	       // User user = userServ.login(newLogin, result);
-	   	User user = userServ.login(newLogin, result);
-	   	
-	       if(result.hasErrors()) {
-	           model.addAttribute("newUser", new User());
-	           model.addAttribute("newLogin", newLogin );
-	           return "index.jsp";
-	       }
-	   
-	       // No errors! 
-	       // TO-DO Later: Store their ID from the DB in session, 
-	       // in other words, log them in.
-	       session.setAttribute("user", user);
-	       return "redirect:/home";
-	   }
-	   
-	   // LOGOUT
-	   @GetMapping("/logout")
-	   public String logout(HttpSession session) {
-	   	session.invalidate();
-	   	return "redirect:/";
-	   }
-	   
+
+	@Autowired
+	private UserService userServ;
+
+	// LOGIN & REGISTRATION
+	@GetMapping("/")
+	public String index(Model model) {
+		// Bind empty User and LoginUser objects to the JSP
+		// to capture the form input
+		model.addAttribute("newUser", new User());
+		model.addAttribute("newLogin", new LoginUser());
+		return "index.jsp";
+	}
+
+	// Registration
+	@PostMapping("/register")
+	public String register(@Valid @ModelAttribute("newUser") User newUser,
+			BindingResult result, Model model, HttpSession session) {
+
+		// TO-DO Later -- call a register method in the service
+		// to do some extra validations and create a new user!
+		userServ.register(newUser, result);
+
+		if (result.hasErrors()) {
+			// Be sure to send in the empty LoginUser before
+			// re-rendering the page.
+			model.addAttribute("newUser", newUser);
+			model.addAttribute("newLogin", new LoginUser());
+			return "index.jsp";
+		}
+
+		// No errors!
+		// TO-DO Later: Store their ID from the DB in session,
+		// in other words, log them in.
+		session.setAttribute("user", newUser);
+		return "redirect:/projects";
+	}
+
+	// Login
+	@PostMapping("/login")
+	public String login(@Valid @ModelAttribute("newLogin") LoginUser newLogin,
+			BindingResult result, Model model, HttpSession session) {
+
+		// Add once service is implemented:
+		// User user = userServ.login(newLogin, result);
+		User user = userServ.login(newLogin, result);
+
+		if (result.hasErrors()) {
+			model.addAttribute("newUser", new User());
+			model.addAttribute("newLogin", newLogin);
+			return "index.jsp";
+		}
+
+		// No errors!
+		// TO-DO Later: Store their ID from the DB in session,
+		// in other words, log them in.
+		session.setAttribute("user", user);
+		return "redirect:/projects";
+	}
+
+	// LOGOUT
+	@GetMapping("/logout")
+	public String logout(HttpSession session) {
+		session.invalidate();
+		return "redirect:/";
+	}
 }
