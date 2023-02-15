cceedbc6d60de2c8a69dcc0e0b6687faa137a60b
Nicholas Ung <37952714+nung22@users.noreply.github.com>
2023-02-14 23:38:14 -0800

    Update TicketService.java

--- src/main/java/com/nicholasung/fleaflickr/services/TicketService.java
+++ src/main/java/com/nicholasung/fleaflickr/services/TicketService.java
@@ -1,12 +1,12 @@
 package com.nicholasung.fleaflickr.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.nicholasung.fleaflickr.models.Ticket;
-import com.nicholasung.fleaflickr.models.User;
 import com.nicholasung.fleaflickr.repositories.TicketRepository;
 import com.nicholasung.fleaflickr.repositories.UserRepository;
 
@@ -20,40 +20,31 @@ public class TicketService {
 	@Autowired
 	UserService userServ;
 	
-	// returns all the tickets
-	public List<Ticket> allTickets(){
-		return ticketRepo.findAll();
-	}
-	// updates a ticket
-	public Ticket updateTicket(Ticket ticket) {
-		return ticketRepo.save(ticket);
-	}
+    // returns all the tickets
+    public List<Ticket> allTickets() {
+        return ticketRepo.findAll();
+    }
     // returns all the tickets for a given project
     public List<Ticket> allProjectTickets(Long id) {
     	return ticketRepo.findByProject_Id(id);
     }
-	// updates a ticket for API
-	public Ticket updateTicket(Long id, String title, String priority, String status,
-			User assignee, String description, String issueType) {
-		Ticket updatedTicket = this.findById(id);
-		updatedTicket.setTitle(title);
-		updatedTicket.setPriority(priority);
-		updatedTicket.setStatus(status);
-		updatedTicket.setAssignee(assignee);
-		updatedTicket.setDescription(description);
-		updatedTicket.setIssueType(issueType);
-		return ticketRepo.save(updatedTicket);
-	}
     // creates a ticket
-	public Ticket createTicket(Ticket ticket) {
-		return ticketRepo.save(ticket);
-	}
-	// deletes a ticket
-	public void deleteTicket(Ticket ticket) {
-		ticketRepo.delete(ticket);
-	}
-	// retrieves a ticket
-	public Ticket findById(Long id) {
-		return ticketRepo.findById(id).orElse(null);
-	}
+    public Ticket createTicket(Ticket b) {
+        return ticketRepo.save(b);
+    }
+    // retrieves a ticket
+    public Ticket findTicket(Long id) {
+    	return ticketRepo.findById(id).orElse(null);
+    }
+    // updates a ticket
+    public Ticket updateTicket(Ticket b) {
+        return ticketRepo.save(b);
+    }
+    // deletes a ticket
+    public void deleteTicket(Long id) {
+    	Optional<Ticket> optionalTicket = ticketRepo.findById(id);
+    	if(optionalTicket.isPresent()) {
+    		ticketRepo.deleteById(id);    		
+    	}
+    }
 }
