d0564b16dfd26b80e481b36febb077b4159a4669
Nicholas Ung <37952714+nung22@users.noreply.github.com>
2023-02-17 17:49:48 -0800

    Update TicketService.java

--- src/main/java/com/nicholasung/fleaflickr/services/TicketService.java
+++ src/main/java/com/nicholasung/fleaflickr/services/TicketService.java
@@ -12,39 +12,44 @@ import com.nicholasung.fleaflickr.repositories.UserRepository;
 
 @Service
 public class TicketService {
-	
-	@Autowired
-	TicketRepository ticketRepo;
-	@Autowired
-	UserRepository userRepo;
-	@Autowired
-	UserService userServ;
-	
+
+    @Autowired
+    TicketRepository ticketRepo;
+    @Autowired
+    UserRepository userRepo;
+    @Autowired
+    UserService userServ;
+
     // returns all the tickets
     public List<Ticket> allTickets() {
         return ticketRepo.findAll();
     }
+
     // returns all the tickets for a given project
     public List<Ticket> allProjectTickets(Long id) {
-    	return ticketRepo.findByProject_Id(id);
+        return ticketRepo.findByProject_Id(id);
     }
+
     // creates a ticket
     public Ticket createTicket(Ticket b) {
         return ticketRepo.save(b);
     }
+
     // retrieves a ticket
     public Ticket findTicket(Long id) {
-    	return ticketRepo.findById(id).orElse(null);
+        return ticketRepo.findById(id).orElse(null);
     }
+
     // updates a ticket
     public Ticket updateTicket(Ticket b) {
         return ticketRepo.save(b);
     }
+
     // deletes a ticket
     public void deleteTicket(Long id) {
-    	Optional<Ticket> optionalTicket = ticketRepo.findById(id);
-    	if(optionalTicket.isPresent()) {
-    		ticketRepo.deleteById(id);    		
-    	}
+        Optional<Ticket> optionalTicket = ticketRepo.findById(id);
+        if (optionalTicket.isPresent()) {
+            ticketRepo.deleteById(id);
+        }
     }
 }
